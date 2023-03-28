23e7442ce51e470177268ec06f6c430de45be27d
AltamirJunio <altamirjuniorpc@gmail.com>
2023-03-27 21:28:18 -0400

    Atualizando codigos 27-03

--- classe/Paciente.class.php
+++ classe/Paciente.class.php
@@ -1,212 +1,254 @@
 <?php
-class Paciente extends Crud{
+class Paciente extends Crud
+{
     protected $tabela = 'Paciente';
     private $idPac;
-private $nomePac;
-private $enderecoPac;
-private $bairroPac;
-private $cidadePac;
+    private $nomePac;
+    private $enderecoPac;
+    private $bairroPac;
+    private $cidadePac;
 
-private $estadoPac;
-private $cepPac;
-private $nascimentoPac;
-private $emailPac;
+    private $estadoPac;
+    private $cepPac;
+    private $nascimentoPac;
+    private $emailPac;
 
-private $celularPac;
-private $fotoPac;
-
-
- 
+    private $celularPac;
+    private $fotoPac;
 
 
 
-	/**
-	 * @return mixed
-	 */
-	public function getIdPac() {
-		return $this->idPac;
-	}
-	
-	/**
-	 * @param mixed $idPac 
-	 * @return self
-	 */
-	public function setIdPac($idPac): self {
-		$this->idPac = $idPac;
-		return $this;
-	}
+    /**
+     * @return mixed
+     */
+    public function getNomePac()
+    {
+        return $this->nomePac;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getNomePac() {
-		return $this->nomePac;
-	}
-	
-	/**
-	 * @param mixed $nomePac 
-	 * @return self
-	 */
-	public function setNomePac($nomePac): self {
-		$this->nomePac = $nomePac;
-		return $this;
-	}
+    /**
+     * @param mixed $nomePac 
+     * @return self
+     */
+    public function setNomePac($nomePac)
+    {
+        $this->nomePac = $nomePac;
+        return $this;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getBairroPac() {
-		return $this->bairroPac;
-	}
-	
-	/**
-	 * @param mixed $bairroPac 
-	 * @return self
-	 */
-	public function setBairroPac($bairroPac): self {
-		$this->bairroPac = $bairroPac;
-		return $this;
-	}
+    /**
+     * @return mixed
+     */
+    public function getBairroPac()
+    {
+        return $this->bairroPac;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getEnderecoPac() {
-		return $this->enderecoPac;
-	}
-	
-	/**
-	 * @param mixed $enderecoPac 
-	 * @return self
-	 */
-	public function setEnderecoPac($enderecoPac): self {
-		$this->enderecoPac = $enderecoPac;
-		return $this;
-	}
+    /**
+     * @param mixed $bairroPac 
+     * @return self
+     */
+    public function setBairroPac($bairroPac)
+    {
+        $this->bairroPac = $bairroPac;
+        return $this;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getCidadePac() {
-		return $this->cidadePac;
-	}
-	
-	/**
-	 * @param mixed $cidadePac 
-	 * @return self
-	 */
-	public function setCidadePac($cidadePac): self {
-		$this->cidadePac = $cidadePac;
-		return $this;
-	}
+    /**
+     * @return mixed
+     */
+    public function getEnderecoPac()
+    {
+        return $this->enderecoPac;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getEstadoPac() {
-		return $this->estadoPac;
-	}
-	
-	/**
-	 * @param mixed $estadoPac 
-	 * @return self
-	 */
-	public function setEstadoPac($estadoPac): self {
-		$this->estadoPac = $estadoPac;
-		return $this;
-	}
+    /**
+     * @param mixed $enderecoPac 
+     * @return self
+     */
+    public function setEnderecoPac($enderecoPac)
+    {
+        $this->enderecoPac = $enderecoPac;
+        return $this;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getCepPac() {
-		return $this->cepPac;
-	}
-	
-	/**
-	 * @param mixed $cepPac 
-	 * @return self
-	 */
-	public function setCepPac($cepPac): self {
-		$this->cepPac = $cepPac;
-		return $this;
-	}
+    /**
+     * @return mixed
+     */
+    public function getCidadePac()
+    {
+        return $this->cidadePac;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getNascimentoPac() {
-		return $this->nascimentoPac;
-	}
-	
-	/**
-	 * @param mixed $nascimentoPac 
-	 * @return self
-	 */
-	public function setNascimentoPac($nascimentoPac): self {
-		$this->nascimentoPac = $nascimentoPac;
-		return $this;
-	}
+    /**
+     * @param mixed $cidadePac 
+     * @return self
+     */
+    public function setCidadePac($cidadePac)
+    {
+        $this->cidadePac = $cidadePac;
+        return $this;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getEmailPac() {
-		return $this->emailPac;
-	}
-	
-	/**
-	 * @param mixed $emailPac 
-	 * @return self
-	 */
-	public function setEmailPac($emailPac): self {
-		$this->emailPac = $emailPac;
-		return $this;
-	}
+    /**
+     * @return mixed
+     */
+    public function getEstadoPac()
+    {
+        return $this->estadoPac;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getCelularPac() {
-		return $this->celularPac;
-	}
-	
-	/**
-	 * @param mixed $celularPac 
-	 * @return self
-	 */
-	public function setCelularPac($celularPac): self {
-		$this->celularPac = $celularPac;
-		return $this;
-	}
+    /**
+     * @param mixed $estadoPac 
+     * @return self
+     */
+    public function setEstadoPac($estadoPac)
+    {
+        $this->estadoPac = $estadoPac;
+        return $this;
+    }
 
-	/**
-	 * @return mixed
-	 */
-	public function getFotoPac() {
-		return $this->fotoPac;
-	}
-	
-	/**
-	 * @param mixed $fotoPac 
-	 * @return self
-	 */
-	public function setFotoPac($fotoPac): self {
-		$this->fotoPac = $fotoPac;
-		return $this;
-	}
-	/**
-	 * @return mixed
-	 */
-	public function iserir() {
-	}
-	
-	/**
-	 *
-	 * @param mixed $campo
-	 * @param mixed $id
-	 * @return mixed
-	 */
-	public function atualizar($campo, $id) {
-	}
+    /**
+     * @return mixed
+     */
+    public function getCepPac()
+    {
+        return $this->cepPac;
+    }
+
+    /**
+     * @param mixed $cepPac 
+     * @return self
+     */
+    public function setCepPac($cepPac)
+    {
+        $this->cepPac = $cepPac;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNascimentoPac()
+    {
+        return $this->nascimentoPac;
+    }
+
+    /**
+     * @param mixed $nascimentoPac 
+     * @return self
+     */
+    public function setNascimentoPac($nascimentoPac)
+    {
+        $this->nascimentoPac = $nascimentoPac;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmailPac()
+    {
+        return $this->emailPac;
+    }
+
+    /**
+     * @param mixed $emailPac 
+     * @return self
+     */
+    public function setEmailPac($emailPac)
+    {
+        $this->emailPac = $emailPac;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCelularPac()
+    {
+        return $this->celularPac;
+    }
+
+    /**
+     * @param mixed $celularPac 
+     * @return self
+     */
+    public function setCelularPac($celularPac)
+    {
+        $this->celularPac = $celularPac;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFotoPac()
+    {
+        return $this->fotoPac;
+    }
+
+    /**
+     * @param mixed $fotoPac 
+     * @return self
+     */
+    public function setFotoPac($fotoPac)
+    {
+        $this->fotoPac = $fotoPac;
+        return $this;
+    }
+    /**
+     * @return mixed
+     */
+    public function iserir()
+    {
+        $nome = $this->getNomePac();
+        $endereco = $this->getEnderecoPac();
+        $bairro = $this->getBairroPac();
+        $cidade = $this->getCidadePac();
+        $estado = $this->getEstadoPac();
+        $cep = $this->getCepPac();
+        $nascimento = $this->getNascimentoPac();
+        $email = $this->getEmailPac();
+        $celular = $this->getCelularPac();
+        $foto = $this->getFotoPac();
+
+        $sqlInserir =
+            "INSERT INTO $this->tabela (nomePac , enderecoPac , bairroPac , cidadePac , estadoPac , cepPac , nascimentoPac , emailPac , celularPac , fotoPac) VALUES (  '$nome', '$endereco', '$bairro', '$cidade', '$estado', '$cep', '$nascimento', '$email', '$celular', '$foto' )";
+
+        if (Conexao::query($sqlInserir)) {
+            header('location: pacientes.php');
+        }
+
+
+    }
+
+    /**
+     *
+     * @param mixed $campo
+     * @param mixed $id
+     * @return mixed
+     */
+    public function atualizar($campo, $id)
+    {
+        $nome = $this->getNomePac();
+        $endereco = $this->getEnderecoPac();
+        $bairro = $this->getBairroPac();
+        $cidade = $this->getCidadePac();
+        $estado = $this->getEstadoPac();
+        $cep = $this->getCepPac();
+        $nascimento = $this->getNascimentoPac();
+        $email = $this->getEmailPac();
+        $celular = $this->getCelularPac();
+        $foto = $this->getFotoPac();
+
+
+        $sqlAtualizar = "UPDATE $this->tabela set nomePac = '$nome', enderecoPac = '$endereco', bairroPac = '$bairro', cidadePac = '$cidade', estadoPac = '$estado', cepPac = '$cep', nascimentoPac ='$nascimento', emailPac= '$email', celularPac ='$celular', fotoPac='$foto'";
+
+        if (Conexao::query($sqlAtualizar)) {
+            header('location: pacientes.php');
+        }
+
+
+    }
 }
--- pacienteGer.php
+++ pacienteGer.php
@@ -9,6 +9,11 @@
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        </nav>
+    </header>
+    <main>
 <div class="container">
 <form class="row g-3" action="<?php echo
 htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"
@@ -79,11 +84,11 @@ name="btnGravar">Gravar</button>
 
 </form>
 </div>
-    
+
 
 <footer>
     </footer>
-
+    </main>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
         crossorigin="anonymous"></script>
