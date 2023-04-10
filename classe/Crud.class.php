11e5b808bc8d7e1f258d7e50ff2c550fb55c81ab
AltamirJunio <altamirjuniorpc@gmail.com>
2023-04-10 19:16:52 -0400

    Aula 09

--- classe/Crud.class.php
+++ classe/Crud.class.php
@@ -3,6 +3,11 @@ abstract class Crud{
     protected $tabela;
     public abstract function iserir();
     public abstract function atualizar($campo, $id);
-    
+
+    public function listar()
+    {
+        $sqlSelect = "select * from {$this->tabela}";
+        return Conexao::query($sqlSelect);
+    }
 
 }
